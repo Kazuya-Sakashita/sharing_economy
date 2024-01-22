d3d155c7ef2a4fd1346a09730f74ed0a69adafcd
Kazuya-Sakashita <kz0508@gmail.com>
2024-01-23 06:41:29 +0900

    19.4

---
+++ config/initializers/locale.rb
@@ -0,0 +1,2 @@
+I18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml→ }")]
+I18n.default_locale = :ja
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    unlocks: 'users/unlocks'
+  }
+
   root to: "home#index"
 end
