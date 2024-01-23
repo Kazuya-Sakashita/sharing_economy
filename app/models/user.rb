a93b267da004940816ce519cd75ff36cc13efd1d
Kazuya-Sakashita <kz0508@gmail.com>
2024-01-24 06:50:49 +0900

    21.16

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :gender
+  end
+
+  enum gender: {
+    unanswered: 0,
+    male: 1,
+    female: 2
+  }
+
+  class << self
+    def genders_i18n
+      I18n.t("enums.user.gender")
+    end
+  end
 end
